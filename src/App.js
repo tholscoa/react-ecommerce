1be1daa13925d9889b8d77bc37601882fe9fb6de
tholscoa <akinnuoyetolulope@gmail.com>
2020-12-17 14:50:16 +0100

    digging into routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.page';
 
+// Remember to import BrowserRouter inside index.js
+import { Route } from 'react-router-dom';
+
+const TestPage = () => (
+  <div>
+    <h1>Test Page </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/testpage' component={TestPage} />
     </div>
   );
 }
