d8fe142d8fa8e44b297fbe57e5285ed89a820903
tholscoa <tholscoa61@yahoo.com>
2020-12-21 00:21:02 +0100

    added filter function to limit ti 4 items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items}) => (
- <div class='collection-preview'>
-   <div class='title'>{title.toUpperCase()}</div>
+const CollectionPreview = ({ title, items }) => (
+  <div class='collection-preview'>
+    <div class='title'>{title.toUpperCase()}</div>
     <div class='preview'>
       {
-        items.map(item => 
+        items.filter((item, idx) => idx < 4).map(item =>
           <div key={item.id}>{item.name}</div>)
       }
     </div>
- </div>
+  </div>
 );
 
 export default CollectionPreview;
