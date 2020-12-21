c7b56bca875a8c6bd3bbca5a0f055195c1757540
tholscoa <tholscoa61@yahoo.com>
2020-12-21 22:57:43 +0100

    added style to sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
   constructor(props) {
