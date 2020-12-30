7c9998783c2f2c4e71f74b797b5d59ccb97fa601
tholscoa <tholscoa61@yahoo.com>
2020-12-30 09:04:16 +0100

    sign in handleSubmit completed

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -17,8 +17,18 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const {email, password}= this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email:'', password:''});
+    } catch (error) {
+      console.log(error)
+    }
+
     this.setState({ email: '', password: '' })
   }
 
@@ -53,7 +63,7 @@ class SignIn extends React.Component {
             required />
 
           <div className='buttons'>
-          <CustomButton type='button'>Sign in</CustomButton>
+          <CustomButton onClick= {this.handleSubmit} type='button'>Sign in</CustomButton>
           <CustomButton onClick={signInWithGoogle} isgooglesignin>Sign in with google</CustomButton>
           </div>
         </form>
